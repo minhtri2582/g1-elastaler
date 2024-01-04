f8ff6f1338c6dda0cbcfca882e568194459d937f
Tri Do Le Minh <minhtri2582@gmail.com>
2024-01-04 11:30:48 +0700

    update elasticsearch

--- src/common/elasticsearch_client.js
+++ src/common/elasticsearch_client.js
@@ -54,37 +54,37 @@ export async function getClientVersion() {
 
     let scheme = 'http';
 
-    if (config.get('es_ssl')) {
-      scheme = 'https';
-    }
+    // if (config.get('es_ssl')) {
+    //   scheme = 'https';
+    // }
 
     let auth = '';
-    
+
     if (config.get('es_username') && config.get('es_password')) {
       auth = `${config.get('es_username')}:${config.get('es_password')}@`;
     }
 
     const agent  = {};
 
-    if (config.get('es_ssl')) {
-      agent.rejectUnauthorized = config.get('ea_verify_certs');
-
-      if (config.get('es_ca_certs')) {
-        agent.ca = fs.readFileSync(config.get('es_ca_certs'));
-      }
-      if (config.get('es_client_cert')) {
-        agent.cert = fs.readFileSync(config.get('es_client_cert'));
-      }
-      if (config.get('es_client_key')) {
-        agent.key = fs.readFileSync(config.get('es_client_key'));
-      }
-    }
+    // if (config.get('es_ssl')) {
+    //   agent.rejectUnauthorized = config.get('ea_verify_certs');
+    //
+    //   if (config.get('es_ca_certs')) {
+    //     agent.ca = fs.readFileSync(config.get('es_ca_certs'));
+    //   }
+    //   if (config.get('es_client_cert')) {
+    //     agent.cert = fs.readFileSync(config.get('es_client_cert'));
+    //   }
+    //   if (config.get('es_client_key')) {
+    //     agent.key = fs.readFileSync(config.get('es_client_key'));
+    //   }
+    // }
 
     const https = require('https');
     const httpsAgent = new https.Agent(agent);
-    const result = 
+    const result =
       await axios.get(
-        `${scheme}://${auth}${config.get('es_host')}:${config.get('es_port')}`, 
+        `${scheme}://${auth}${config.get('es_host')}:${config.get('es_port')}`,
         {httpsAgent}
       );
     return parseInt(result.data.version['number'].split('.')[0], 10);
@@ -171,23 +171,23 @@ export async function getClient() {
     let scheme = 'http';
     let ssl_body = {};
 
-    if (config.get('es_ssl')) {
-      scheme = 'https';
-      ssl_body.rejectUnauthorized = config.get('ea_verify_certs');
-
-      if (config.get('es_ca_certs')) {
-        ssl_body.ca = fs.readFileSync(config.get('es_ca_certs'));
-      }
-      if (config.get('es_client_cert')) {
-        ssl_body.cert = fs.readFileSync(config.get('es_client_cert'));
-      }
-      if (config.get('es_client_key')) {
-        ssl_body.key = fs.readFileSync(config.get('es_client_key'));
-      }
-    }
+    // if (config.get('es_ssl')) {
+    //   scheme = 'https';
+    //   ssl_body.rejectUnauthorized = config.get('ea_verify_certs');
+    //
+    //   if (config.get('es_ca_certs')) {
+    //     ssl_body.ca = fs.readFileSync(config.get('es_ca_certs'));
+    //   }
+    //   if (config.get('es_client_cert')) {
+    //     ssl_body.cert = fs.readFileSync(config.get('es_client_cert'));
+    //   }
+    //   if (config.get('es_client_key')) {
+    //     ssl_body.key = fs.readFileSync(config.get('es_client_key'));
+    //   }
+    // }
 
     let auth = '';
-    
+
     if (config.get('es_username') && config.get('es_password')) {
       auth = `${config.get('es_username')}:${config.get('es_password')}@`;
     }
@@ -199,7 +199,7 @@ export async function getClient() {
         node: [ `${scheme}://${auth}${config.get('es_host')}:${config.get('es_port')}`],
         ssl: ssl_body
       });
-      return client_opensearch;    
+      return client_opensearch;
     }
 
     if (es_version === 5) {
@@ -211,15 +211,15 @@ export async function getClient() {
       });
       return client5;
     } else if (es_version == 6) {
-      
+
       // Elasticsearch 6.x
       const client6 = new elasticsearch6.Client({
         node: [ `${scheme}://${auth}${config.get('es_host')}:${config.get('es_port')}`],
         ssl: ssl_body
       });
       return client6;
-    } else if (es_version == 7) {      
-      
+    } else if (es_version == 7) {
+
       // Elasticsearch 7.x
       const client7 = new elasticsearch7.Client({
         node: [ `${scheme}://${auth}${config.get('es_host')}:${config.get('es_port')}`],
@@ -227,7 +227,7 @@ export async function getClient() {
       });
       return client7;
     } else if (es_version == 8) {
-      
+
       // Elasticsearch 8.x
       const client8 = new elasticsearch8.Client({
         node: [ `${scheme}://${auth}${config.get('es_host')}:${config.get('es_port')}`],
